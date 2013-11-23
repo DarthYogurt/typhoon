6f1c070c4ff79cf734e099759d192e9c0a4371fa
Raymond Walintukan <raytochina@gmail.com>
2013-11-23 14:20:17 -0800

    redone Manifest

--- src/com/TestAndroid/typhoon/MainActivity.java
+++ src/com/TestAndroid/typhoon/MainActivity.java
@@ -1,10 +1,16 @@
 package com.TestAndroid.typhoon;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	static final String EXTRA_MESSAGE = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,19 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void callToastBox(View view){
+		Toast.makeText(this, "Toastbox called", Toast.LENGTH_LONG).show();
+	}
+	
+	public void sendMessage(View view){
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText batmanText = (EditText) findViewById(R.id.batmanText);
+		String message = batmanText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		
+		
+	}
 
+	
 }
